9e75709da011f3cafde6cb1a95c8271c3fc05a3b
ortiie7 <83276413+ortiie7@users.noreply.github.com>
2023-04-30 12:41:24 +0200

    update liste invités

--- mariage/script.js
+++ mariage/script.js
@@ -1,6 +1,29 @@
 /*import CryptoJS from "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/sha1.js";*/
 var n=2; 	
 
+let liste_clair_avec = ['Pauline Maigne','Jean-Etienne Maigne','Caroline Leroux','Cédric Leroux','Marie-Aline Maigne','Pierre Maigne',
+'Catherine Houël','Louis Poissenot','Anne Maigne','Jean-François Maigne','Denise Portail','Albert Portail','Jean-François Ahr','Nathalie Portail',
+'Catherine Ahr','Céline Bachelier','Alain Bachelier','Oriane Leroux','Nicolas Houël','Sandrine Houël','Christine Nedelec','Frédéric Nedelec',
+'Sébastien Houël','Caroline Houël','Jean-Guillaume Houël','Sophie Houël','Gildas Houël','Estelle Houël','Antoine Houël','Diane Begard',
+'Frédéric Maigne','Delphine Maigne','Olivier Maigne','Sophie Maigne','Clémentine Bachelier','Margaux Bachelier','Charles Bailly','Grégoire Bailly',
+'Hugo Metteil','Jacques Vignaud','Frédérique Vignaud','François Vignaud','Alexandra Houël','Oriane Houël','Gabriel Nedelec','Yvan Nedelec',
+'Anaïs Houël','Jean-Baptiste Membrado','Adrien Nivaggioli','Stanislas Barthélémy','Adrien Dumarquez','Antonin Berthon','Eduardo Vitral',
+'Maxime Roméas','Sophie Duval','Aliénor Winum','Camille Tixier','Ons Hiver Tiss','Joannice Hiver Tiss','Alice Glémarec','Quentin Glémarec',
+'Catherine Thévenet','Mathieu Fouquet','Baptiste Brossard','Michel Gabagnou','Catherine Gabagnou','Damien Colombo','Sophie Colombo',
+'Amélie Raby','Fabienne Hausermann','Jean Hausermann','Stéphanie Gander'
+ ] ;
+let liste_clair_sans = ['Gaspard Maigne','Robert Maigne','Capucine Leroux','Jacqueline Leroux','Emmanuelle Maigne','Margot Ahr','Arthur Ahr',
+'Maxime Bailly','Camille Metteil','Elisabeth Houël','Maximilien Houël','Valentine Houël','Lucas Nedelec','Angèle Nedelec','Ninon Houël',
+'Avril Houël','Joseph Houël','Manon Houël','Robin Houël','Lily Houël','Léopoldine Houël','Léopoldine Maigne','Côme Maigne','Cyprien Maigne',
+'Blanche Maigne','Amicie Maigne','Ferréol Maigne','Antoine Maigne','Joséphine Maigne','Adrien Maigne','Itri Vacchiani','Hugo Bazille',
+'Paul-Emile Landrin','Lucas Journel','Sébastien Gomé','Louis Reboul','Félix Martel','Pablo Le Hénaff','Simon Msika','Alexandre Matton',
+'Luka Selem','Sabine Gossellin','Céline Perrot','Henry de Catheu','Hugo Mlodecki','Victor Poyard','Julien Naty-Daufin','Loreline Faugier',
+'Anne-Charlotte Gabagnou','Marie-Sophie Gabagnou','Victoire Gabagnou','Pierre Colombo','Julien Colombo','Carole Colombo','Josiane Cannioux'
+] ;
+
+
+
+
 let invite_sans_acc = ['Toto', 'Titi'];
 let invite_avec_acc = ['d9c5ce191ad566e4751c3fadb7cd27a0dbf4910d','dec450a0be56389a6a68d114eed5714f1ff2c222'];
 
